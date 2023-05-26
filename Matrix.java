ae3ef761e8a88530704532a3c19fd4c2e8408b95
Dinner-David <Gabriel.Dinner-David@student.tdsb.on.ca>
2023-05-26 09:40:58 -0400

    no returns in loops

--- Matrix.java
+++ Matrix.java
@@ -1,27 +1,29 @@
 public class Matrix {
     public static int checkWinner(int[][] board) {
-        int[][] positions = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-        for (int i = 0; i < positions.length; i++) {
-            int a = get(board, positions[i][0]);
-            int b = get(board, positions[i][1]);
-            int c = get(board, positions[i][2]);
-            if (a != Globals.NO_PLAYER && a == b && a == c) {
-                return a;
-            }
-        }
-        return full(board) ? Globals.GAME_TIE : Globals.GAME_STILL_ON;
+	int ret = get(board, 5) != Globals.NO_PLAYER ? Globals.GAME_TIE : Globals.GAME_STILL_ON;
+	int[][] positions = new int[][]{{1, 0, 2}, {4, 3, 5}, {8, 7, 6}, {3, 0, 6}, {7, 4, 1}, {2, 5, 8}, {0, 4, 8}, {6, 4, 2}};
+	for (int i = 0; i < positions.length; i++) {
+	    int a = get(board, positions[i][0]);
+	    ret = ret == Globals.GAME_TIE && a != Globals.NO_PLAYER ? Globals.GAME_TIE : Globals.GAME_STILL_ON;
+	    int b = get(board, positions[i][1]);
+	    int c = get(board, positions[i][2]);
+	    if (a != Globals.NO_PLAYER && a == b && a == c) {
+		ret = a;
+	    }
+	}
+	return ret;
     }
 
     public static int get(int[][] board, int n) {
-        return board[n / 3][n % 3];
+	return board[n / 3][n % 3];
     }
 
     public static boolean full(int[][] board) {
-        for (int i = 0; i < 9; i++) {
-            if (get(board, i) == Globals.NO_PLAYER) {
-                return false;
-            }
-        }
-        return true;
+	for (int i = 0; i < 9; i++) {
+	    if (get(board, i) == Globals.NO_PLAYER) {
+		return false;
+	    }
+	}
+	return true;
     }
 }
